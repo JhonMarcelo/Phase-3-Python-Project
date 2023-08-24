f67de60e9a3bd28d99491c2ae0f4127837d082fa
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-08-23 23:12:21 -0600

    made schema for the users table, used alembic revision and head to create the table at the users.db

--- models.py
+++ models.py
@@ -1,9 +1,21 @@
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
 
 
 Base = declarative_base()
 
 # SCHEMA
 class User(Base):
-    __tablename__ = 'user'
-    pass
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True )
+    username = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    weight = Column(Integer)
+    goal = Column(Integer)
+    target_calorie = Column(Integer)
+    current_calorie = Column(Integer)
+
+
+
