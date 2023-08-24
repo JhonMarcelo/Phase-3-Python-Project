3de4fb7535f0234c606ea8a82def445bdf3be2ff
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-08-23 22:49:12 -0600

    modify env.py and imported base from models, aded target_metadata and created the models.py file

--- migrations/env.py
+++ migrations/env.py
@@ -13,12 +13,12 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-
+ 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from models import Base
+target_metadata = Base.metadata
+# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
