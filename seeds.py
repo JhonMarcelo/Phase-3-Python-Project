cd1f5195b1045d2e419113cbc89cfae6790c6b17
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-08-24 01:24:33 -0600

    install requests

--- seeds.py
+++ seeds.py
@@ -1,6 +1,11 @@
 from models import User, Food   # You will need to import your own models
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+# For generating Fake data: https://faker.readthedocs.io/en/master/providers.html
+from faker import Faker
+#add this line so can use fake. somthing
+fake = Faker()
+
 
 engine = create_engine("sqlite:///data.db")
 Session = sessionmaker(bind=engine)
@@ -9,17 +14,14 @@ session = Session() # Query the DB with session example: session.query(ModelOne)
 # Use ipdb to interact with DB using session
 import ipdb; ipdb.set_trace() # # Dont forget to add ipdb as a dependency - pipenv install ipdb
 
-# For generating Fake data: https://faker.readthedocs.io/en/master/providers.html
-from faker import Faker
-#add this line so can use fake. somthing
-fake = Faker()
+
 
 # For working with an API and retrieving json data
-import requests
-import json
+# import requests
+# import json
 
 print("Seeding...")
-
+import ipdb; ipdb.set_trace()
 for _ in range(20):
 
     first_name=fake.first_name()
