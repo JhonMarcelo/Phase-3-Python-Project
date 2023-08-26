a506f8368a705ed0705e11c25c8df686c028b209
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-08-26 01:27:23 -0600

    add user goal and calculate current user target_calorie for the day

--- cli.py
+++ cli.py
@@ -9,12 +9,13 @@ import time
 class CLI():
 
     def __init__(self):
-        current_user = None
+        current_user = None #Done
         current_user_calorie = 0
-        current_user_target_calorie = 0
-        current_user_weight = 0
-        current_user_activity = 0
-        current_user_goal = 0
+        current_user_target_calorie = 0 #Done
+        current_user_weight = 0 #Done
+        current_user_activity = 0 #Done
+        current_user_goal = 0 #Done
+        activity = 0
 
     def start(self):
         self.clear_screen(44)
@@ -49,19 +50,62 @@ class CLI():
         
     def user_activity_level(self):
         self.clear_screen(44)
-        print("Enter your activity level:\n1 - Sedentary \n2 - Lightly - Active \n3 - Active \n4 - Very Active")
+        print("Enter your activity level:\n\n1 - Sedentary \n2 - Lightly - Active \n3 - Active \n4 - Very Active\n\n")
         options = ["1","2","3","4"]
         terminal_menu = TerminalMenu(options)
         menu_entry_index = terminal_menu.show()
 
         if options[menu_entry_index] == "1":
-            self.user_activity_level = 1
+            self.current_user_activity = 1
+            self.activity = -4
+
+            time.sleep(1)
+            self.user_goal()
         if options[menu_entry_index] == "2":
-            self.user_activity_level = 2
+            self.current_user_activity = 2
+            self.activity = -2
+
+            time.sleep(1)
+            self.user_goal()
         if options[menu_entry_index] == "3":
-            self.user_activity_level = 3
+            self.current_user_activity = 3
+            self.activity = 0
+
+            time.sleep(1)
+            self.user_goal()
         if options[menu_entry_index] == "4":
-            self.user_activity_level = 4
+            self.current_user_activity = 4
+            self.activity = 4
+
+            time.sleep(1)
+            self.user_goal()
+
+    def user_goal(self):
+        self.clear_screen(44)
+        print("What is your goal:\n\n1 - Lose Weight\n2 - Maintain Weight\n3 - Gain Weight\n\n")
+        options = ["1","2","3"]
+        terminal_menu = TerminalMenu(options)
+        menu_entry_index = terminal_menu.show()
+
+        if options[menu_entry_index] == "1":
+            self.current_user_goal = 1
+            self.current_user_target_calorie = self.current_user_weight * (13 + self.activity)
+
+        if options[menu_entry_index] == "2":
+            self.current_user_goal = 2
+            self.current_user_target_calorie = self.current_user_weight * (15 + self.activity)
+
+        if options[menu_entry_index] == "3":
+            self.current_user_goal = 3
+            self.current_user_target_calorie = self.current_user_weight * (17 + self.activity)
+        
+        # import ipdb; ipdb.set_trace()
+
+
+    def user_interface(self):
+        pass:
+
+ 
 
     def exit(self):
         print("Consistency is the KEY! See ya!")
@@ -69,5 +113,6 @@ class CLI():
     def clear_screen(self, lines):
         print("\n"*lines)
 
+    
 app = CLI()
 app.start()
